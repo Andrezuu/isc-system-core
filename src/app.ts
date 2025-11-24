d130f1eb6ad98f3f046df0bfefc1caa6594af369
ObiTobi64 <obitotcp@gmail.com>
2025-11-24 07:22:18 -0400

    fix

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,9 @@ dotenv.config();
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
